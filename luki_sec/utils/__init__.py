dab876038017d1d3bb5c1931b99fcb6e253a2b51
zkr99 <entercommercial888@gmail.com>
2025-12-30 21:30:43 +0100

    Update utils init to export new validator functions

--- luki_sec/utils/__init__.py
+++ luki_sec/utils/__init__.py
@@ -4,10 +4,32 @@ ID generation, validation, and helper functions
 """
 
 from .ids import generate_user_id, generate_session_id, generate_trace_id, validate_id
+from .validators import (
+    validate_user_id as validate_user_id_format,
+    validate_scope,
+    validate_scopes,
+    validate_consent_status,
+    validate_privacy_level,
+    validate_key_id,
+    validate_encryption_data,
+    validate_expiry_days,
+    sanitize_audit_message,
+)
 
 __all__ = [
+    # ID generation
     "generate_user_id",
     "generate_session_id", 
     "generate_trace_id",
     "validate_id",
+    # Validators
+    "validate_user_id_format",
+    "validate_scope",
+    "validate_scopes",
+    "validate_consent_status",
+    "validate_privacy_level",
+    "validate_key_id",
+    "validate_encryption_data",
+    "validate_expiry_days",
+    "sanitize_audit_message",
 ]
