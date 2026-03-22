c17f572f845a91cb79dbc2843d58b8460d7c59e4
zkr99 <entercommercial888@gmail.com>
2026-03-22 21:10:12 +0100

    Remove stray git commit message from test file docstring

--- tests/test_encrypt_endpoints.py
+++ tests/test_encrypt_endpoints.py
@@ -1,4 +1,4 @@
-git commit -m "Stabilise encrypt/decrypt endpoint tests by initialising encryption service""""Tests for /encrypt and /decrypt endpoints in the security-privacy service."""
+"""Tests for /encrypt and /decrypt endpoints in the security-privacy service."""
 
 from __future__ import annotations
 
